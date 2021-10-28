e06752f7244e5e96ebf38a4326d9921cbe6367a4
diazv <75600392+victorzdev@users.noreply.github.com>
2021-10-28 00:09:05 -0400

    add homepage and placeholder content

---
+++ front-page.php
@@ -0,0 +1,356 @@
+<?php get_header(); ?>
+
+<div class="container mx-auto my-8">
+
+  <div class="relative">
+    <div aria-hidden="true" class="hidden absolute w-1/2 h-full bg-gray-100 lg:block"></div>
+    <div class="relative bg-gray-100 lg:bg-transparent">
+      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 lg:grid lg:grid-cols-2">
+        <div class="max-w-2xl mx-auto py-12 lg:py-32 lg:max-w-none">
+          <div class="lg:pr-16">
+            <h1 class="font-serif text-4xl text-gray-900 sm:text-5xl xl:text-6xl">The perfect gift for the season</h1>
+            <p class="mt-4 text-xl text-gray-600">Browse our collection of orchids guaranteed to be blooming size.</p>
+            <div class="mt-6">
+              <a href="#" class="inline-block bg-pink-600 border border-transparent py-3 px-8 rounded-md font-medium text-white hover:bg-pink-700">Shop gifts</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="w-full h-48 sm:h-64 lg:absolute lg:top-0 lg:right-0 lg:w-1/2 lg:h-full">
+      <img src="http://exotica.local/wp-content/uploads/2021/10/orchid-hero.jpg" alt="" class="w-full h-full object-center object-cover">
+    </div>
+  </div>
+
+  <!-- Collections  -->
+  <section aria-labelledby="collections-heading">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="max-w-2xl mx-auto py-8 sm:py-24 lg:py-12 lg:max-w-none">
+        <div class="mt-6 space-y-12 lg:space-y-0 lg:grid lg:grid-cols-3 lg:gap-x-6">
+          <div class="group relative">
+            <div class="relative w-full h-80 bg-white rounded-lg overflow-hidden group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1">
+              <img src="http://exotica.local/wp-content/uploads/2021/10/il_1588xN.3095096713_e3lz.jpg" alt="Collection of four insulated travel bottles on wooden shelf." class="w-full h-full object-center object-cover">
+            </div>
+            <h3 class="mt-2 font-semibold text-gray-900">
+              <a href="#">
+                Cattleya Alliance
+              </a>
+            </h3>
+          </div>
+
+          <div class="group relative">
+            <div class="relative w-full h-80 bg-white rounded-lg overflow-hidden group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1">
+              <img src="http://exotica.local/wp-content/uploads/2021/10/il_1588xN.2709683534_866j.jpg" alt="Collection of four insulated travel bottles on wooden shelf." class="w-full h-full object-center object-cover">
+            </div>
+            <h3 class="mt-2 font-semibold text-gray-900">
+              <a href="#">
+                Dendrobium
+              </a>
+            </h3>
+          </div>
+
+          <div class="group relative">
+            <div class="relative w-full h-80 bg-white rounded-lg overflow-hidden group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1">
+              <img src="http://exotica.local/wp-content/uploads/2021/10/il_1588xN.2620804155_lvlo.jpg" alt="Collection of four insulated travel bottles on wooden shelf." class="w-full h-full object-center object-cover">
+            </div>
+            <h3 class="mt-2 font-semibold text-gray-900">
+              <a href="#">
+                Oncidium
+              </a>
+            </h3>
+          </div>
+        </div>
+        <div class="mt-6 space-y-12 lg:space-y-0 lg:grid lg:grid-cols-3 lg:gap-x-6">
+          <div class="group relative">
+            <div class="relative w-full h-80 bg-white rounded-lg overflow-hidden group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1">
+              <img src="http://exotica.local/wp-content/uploads/2021/10/il_1588xN.2650122036_56jn.jpg" alt="Collection of four insulated travel bottles on wooden shelf." class="w-full h-full object-center object-cover">
+            </div>
+            <h3 class="mt-2 font-semibold text-gray-900">
+              <a href="#">
+                Slipper Orchids
+              </a>
+            </h3>
+          </div>
+
+          <div class="group relative">
+            <div class="relative w-full h-80 bg-white rounded-lg overflow-hidden group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1">
+              <img src="http://exotica.local/wp-content/uploads/2021/10/il_1588xN.2848386455_sz7n.jpg" alt="Collection of four insulated travel bottles on wooden shelf." class="w-full h-full object-center object-cover">
+            </div>
+            <h3 class="mt-2 font-semibold text-gray-900">
+              <a href="#">
+                Vanda
+              </a>
+            </h3>
+          </div>
+
+          <div class="group relative">
+            <div class="relative w-full h-80 bg-white rounded-lg overflow-hidden group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1">
+              <img src="http://exotica.local/wp-content/uploads/2021/10/il_1588xN.3106380612_8t3i.jpg" alt="Collection of four insulated travel bottles on wooden shelf." class="w-full h-full object-center object-cover">
+            </div>
+            <h3 class="mt-2 font-semibold text-gray-900">
+              <a href="#">
+                Miscellaneous Orchids
+              </a>
+            </h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Trending products -->
+  <section aria-labelledby="trending-heading" class="bg-white">
+    <div class="py-12 sm:py-24 lg:max-w-7xl lg:mx-auto lg:py-8 lg:px-8">
+      <div class="px-4 flex items-center justify-between sm:px-6 lg:px-0">
+        <h2 id="trending-heading" class="font-serif text-3xl text-gray-900">New arrivals</h2>
+        <a href="#" class="hidden sm:block text-sm font-semibold text-indigo-600 hover:text-indigo-500">Shop all<span aria-hidden="true"> &rarr;</span></a>
+      </div>
+
+      <div class="mt-8 relative">
+        <div class="relative w-full overflow-x-auto">
+          <ul role="list" class="mx-4 inline-flex space-x-8 sm:mx-6 lg:mx-0 lg:space-x-0 lg:grid lg:grid-cols-4 lg:gap-x-8">
+            <li class="w-64 inline-flex flex-col text-center lg:w-auto">
+              <div class="group relative">
+                <div class="w-full bg-gray-200 rounded-md overflow-hidden aspect-w-1 aspect-h-1">
+                  <img src="http://exotica.local/wp-content/uploads/2021/10/il_1588xN.2578358290_302s.jpg" alt="Black machined steel pen with hexagonal grip and small white logo at top." class="w-full h-full object-center object-cover group-hover:opacity-75">
+                </div>
+                <div class="mt-6">
+                  <h3 class="mt-1 font-semibold text-gray-900">
+                    <a href="#">
+                      <span class="absolute inset-0"></span>
+                      Dendrobium Blue Twinkle
+                    </a>
+                  </h3>
+                  <p class="mt-1 text-gray-900">
+                    $65
+                  </p>
+                </div>
+              </div>
+            </li>
+            <li class="w-64 inline-flex flex-col text-center lg:w-auto">
+              <div class="group relative">
+                <div class="group relative">
+                  <div class="w-full bg-gray-200 rounded-md overflow-hidden aspect-w-1 aspect-h-1">
+                    <img src="http://exotica.local/wp-content/uploads/2021/10/il_1588xN.2658921170_d0zg.jpg" alt="Black machined steel pen with hexagonal grip and small white logo at top." class="w-full h-full object-center object-cover group-hover:opacity-75">
+                  </div>
+                  <div class="mt-6">
+                    <h3 class="mt-1 font-semibold text-gray-900">
+                      <a href="#">
+                        <span class="absolute inset-0"></span>
+                        Cattleya Violacea
+                      </a>
+                    </h3>
+                    <p class="mt-1 text-gray-900">
+                      $45
+                    </p>
+                  </div>
+                </div>
+            </li>
+            <li class="w-64 inline-flex flex-col text-center lg:w-auto">
+              <div class="group relative">
+                <div class="group relative">
+                  <div class="w-full bg-gray-200 rounded-md overflow-hidden aspect-w-1 aspect-h-1">
+                    <img src="http://exotica.local/wp-content/uploads/2021/10/il_1588xN.2953661255_jm99.jpg" alt="Black machined steel pen with hexagonal grip and small white logo at top." class="w-full h-full object-center object-cover group-hover:opacity-75">
+                  </div>
+                  <div class="mt-6">
+                    <h3 class="mt-1 font-semibold text-gray-900">
+                      <a href="#">
+                        <span class="absolute inset-0"></span>
+                        Papilionanthe Teres
+                      </a>
+                    </h3>
+                    <p class="mt-1 text-gray-900">
+                      $32
+                    </p>
+                  </div>
+                </div>
+            </li>
+            <li class="w-64 inline-flex flex-col text-center lg:w-auto">
+              <div class="group relative">
+                <div class="group relative">
+                  <div class="w-full bg-gray-200 rounded-md overflow-hidden aspect-w-1 aspect-h-1">
+                    <img src="http://exotica.local/wp-content/uploads/2021/10/il_1588xN.2904797560_7k69.jpg" alt="Black machined steel pen with hexagonal grip and small white logo at top." class="w-full h-full object-center object-cover group-hover:opacity-75">
+                  </div>
+                  <div class="mt-3 mb-6">
+                    <h3 class="mt-1 font-semibold text-gray-900">
+                      <a href="#">
+                        <span class="absolute inset-0"></span>
+                        Paphiopedilum Maudiae
+                      </a>
+                    </h3>
+                    <p class="mt-1 text-gray-900">
+                      $39
+                    </p>
+                  </div>
+                </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="mt-8 px-4 sm:hidden">
+        <a href="#" class="text-sm font-semibold text-indigo-600 hover:text-indigo-500">Shop all<span aria-hidden="true"> &rarr;</span></a>
+      </div>
+    </div>
+  </section>
+
+  <!-- Testimonials  -->
+  <section aria-labelledby="testimonial-heading" class="relative py-6 max-w-7xl mx-auto px-4 sm:px-6 lg:py-6 lg:px-8">
+    <div class="max-w-2xl mx-auto lg:max-w-none">
+      <h2 id="testimonial-heading" class="font-serif text-3xl text-gray-900">What are people saying?</h2>
+
+      <div class="mt-8 space-y-16 lg:space-y-0 lg:grid lg:grid-cols-3 lg:gap-x-8">
+        <blockquote class="sm:flex lg:block">
+          <svg width="24" height="18" viewBox="0 0 24 18" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" class="flex-shrink-0 text-gray-300">
+            <path d="M0 18h8.7v-5.555c-.024-3.906 1.113-6.841 2.892-9.68L6.452 0C3.188 2.644-.026 7.86 0 12.469V18zm12.408 0h8.7v-5.555C21.083 8.539 22.22 5.604 24 2.765L18.859 0c-3.263 2.644-6.476 7.86-6.451 12.469V18z" fill="currentColor" />
+          </svg>
+          <div class="mt-8 sm:mt-0 sm:ml-6 lg:mt-10 lg:ml-0">
+            <p class="text-lg text-gray-600">
+              A much larger plant than I was anticipating I’m ecstatic! Been wanting a tibinicis since I was 20 years old… that was 20 years ago.
+            </p>
+            <cite class="mt-4 block font-semibold not-italic text-gray-900">
+              Jared
+            </cite>
+          </div>
+        </blockquote>
+
+        <blockquote class="sm:flex lg:block">
+          <svg width="24" height="18" viewBox="0 0 24 18" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" class="flex-shrink-0 text-gray-300">
+            <path d="M0 18h8.7v-5.555c-.024-3.906 1.113-6.841 2.892-9.68L6.452 0C3.188 2.644-.026 7.86 0 12.469V18zm12.408 0h8.7v-5.555C21.083 8.539 22.22 5.604 24 2.765L18.859 0c-3.263 2.644-6.476 7.86-6.451 12.469V18z" fill="currentColor" />
+          </svg>
+          <div class="mt-8 sm:mt-0 sm:ml-6 lg:mt-10 lg:ml-0">
+            <p class="text-lg text-gray-600">
+              Arrived quickly and in good condition! I was very happy with my plant and will shop at this shop again in the future. Looked like a nice place to get some really neat plants!
+            </p>
+            <cite class="mt-4 block font-semibold not-italic text-gray-900">
+              Bobby
+            </cite>
+          </div>
+        </blockquote>
+
+        <blockquote class="sm:flex lg:block">
+          <svg width="24" height="18" viewBox="0 0 24 18" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" class="flex-shrink-0 text-gray-300">
+            <path d="M0 18h8.7v-5.555c-.024-3.906 1.113-6.841 2.892-9.68L6.452 0C3.188 2.644-.026 7.86 0 12.469V18zm12.408 0h8.7v-5.555C21.083 8.539 22.22 5.604 24 2.765L18.859 0c-3.263 2.644-6.476 7.86-6.451 12.469V18z" fill="currentColor" />
+          </svg>
+          <div class="mt-8 sm:mt-0 sm:ml-6 lg:mt-10 lg:ml-0">
+            <p class="text-lg text-gray-600">
+              My Stanhopea came to me in great shape. It was exactly as pictured and packed very well. Thank you so much!
+            </p>
+            <cite class="mt-4 block font-semibold not-italic text-gray-900">
+              Andrea
+            </cite>
+          </div>
+        </blockquote>
+      </div>
+    </div>
+  </section>
+
+  <!-- About us  -->
+  <section class="relative bg-white pt-6 pb-6 overflow-hidden">
+    <div class="mt-12">
+      <div class="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
+
+        <div class="mt-12 sm:mt-16 lg:mt-0">
+          <div class="lg:relative lg:h-full">
+            <img class="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5" src="https://gomezexotica.square.site/uploads/b/1b66bef7e7ca54a1c93f1ad13184472c2294c17364557f830ecf499df9a6ea7c/IMG_8995_1604169601.JPG?width=800" alt="Customer profile user interface">
+          </div>
+        </div>
+        <div class="px-4 max-w-xl mx-auto sm:px-6 lg:max-w-none lg:mx-0 lg:px-0">
+          <div>
+            <div class="mt-6">
+              <h2 class="font-serif text-3xl text-gray-900">
+                Superior service, personalized attention
+              </h2>
+              <p class="mt-4 text-lg text-gray-500">
+                We're proud to offer the highest quality, most unique orchids on the market today. We put careful attention into each plant listed, from growing seedling populations to awarded specimens, to packing and shipping. We hope you enjoy our plants as much as we enjoy sharing them with you.
+              </p>
+              <p class="mt-4 text-lg text-gray-500">
+                This picture reminds us of where it all began, day one of renting our greenhouse. Let your imagination fill the space now that we propagate thousands of species and hybrids.
+              </p>
+              <div class="mt-6">
+                <a href="#" class="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-pink-600 hover:bg-pink-700">
+                  Learn more
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Perks -->
+  <section aria-labelledby="perks-heading" class="relative mt-6 bg-gray-50">
+    <h2 id="perks-heading" class="sr-only">Our perks</h2>
+    <div class="py-6 max-w-7xl mx-auto px-4 sm:px-6 lg:py-6 lg:px-8">
+      <div class="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 lg:gap-x-8 lg:gap-y-0">
+        <div class="text-center md:flex md:items-start md:text-left lg:block lg:text-center">
+          <div class="md:flex-shrink-0">
+            <div class="flow-root">
+              <!-- <img class="-my-1 h-24 w-auto mx-auto" src="https://tailwindui.com/img/ecommerce/icons/icon-returns-light.svg" alt=""> -->
+            </div>
+          </div>
+          <div class="mt-6 md:mt-0 md:ml-4 lg:mt-6 lg:ml-0">
+            <h3 class="text-sm font-semibold tracking-wide uppercase text-gray-900">
+              Shop with Confidence
+            </h3>
+            <p class="mt-3 text-sm text-gray-500">
+              We've helped hundreds of customers grow beautiful orchids. We also have a 5-star rating on Etsy!
+            </p>
+          </div>
+        </div>
+
+        <div class="text-center md:flex md:items-start md:text-left lg:block lg:text-center">
+          <div class="md:flex-shrink-0">
+            <div class="flow-root">
+              <!-- <img class="-my-1 h-24 w-auto mx-auto" src="https://tailwindui.com/img/ecommerce/icons/icon-calendar-light.svg" alt=""> -->
+            </div>
+          </div>
+          <div class="mt-6 md:mt-0 md:ml-4 lg:mt-6 lg:ml-0">
+            <h3 class="text-sm font-semibold tracking-wide uppercase text-gray-900">
+              Rare Species & Colors
+            </h3>
+            <p class="mt-3 text-sm text-gray-500">
+              We offer a wide selection of rare orchids and unique colors to add a splash of color to your garden or home.
+            </p>
+          </div>
+        </div>
+
+        <div class="text-center md:flex md:items-start md:text-left lg:block lg:text-center">
+          <div class="md:flex-shrink-0">
+            <div class="flow-root">
+              <!-- <img class="-my-1 h-24 w-auto mx-auto" src="https://tailwindui.com/img/ecommerce/icons/icon-gift-card-light.svg" alt=""> -->
+            </div>
+          </div>
+          <div class="mt-6 md:mt-0 md:ml-4 lg:mt-6 lg:ml-0">
+            <h3 class="text-sm font-semibold tracking-wide uppercase text-gray-900">
+              Expert Advice
+            </h3>
+            <p class="mt-3 text-sm text-gray-500">
+              We’re always happy to make growing suggestions and help you along the way.
+            </p>
+          </div>
+        </div>
+
+        <div class="text-center md:flex md:items-start md:text-left lg:block lg:text-center">
+          <div class="md:flex-shrink-0">
+            <div class="flow-root">
+              <!-- <img class="-my-1 h-24 w-auto mx-auto" src="https://tailwindui.com/img/ecommerce/icons/icon-planet-light.svg" alt=""> -->
+            </div>
+          </div>
+          <div class="mt-6 md:mt-0 md:ml-4 lg:mt-6 lg:ml-0">
+            <h3 class="text-sm font-semibold tracking-wide uppercase text-gray-900">
+              Fast & Free Shipping
+            </h3>
+            <p class="mt-3 text-sm text-gray-500">
+              We ship our plants straight from our store to your front door. We'll ensure only top quality plants ship directly to you.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+</div>
+
+<?php
+get_footer();
